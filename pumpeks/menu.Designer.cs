cdc919bf627661b1c9edfd431f070af10648f52a
koisix <139511734+koisix@users.noreply.github.com>
2023-09-03 04:27:34 +0300

    Proje dosyası ekle.

---
+++ pumpeks/menu.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace pumpeks
+{
+    partial class menu
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.boyut = new Guna.UI2.WinForms.Guna2TextBox();
+            this.birim = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.pompala = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.ustuneEkle = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Tüm Dosyalar|*.*";
+            this.openFileDialog1.Title = "Dosya Se.";
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(224, 57);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(129, 42);
+            this.guna2HtmlLabel1.TabIndex = 6;
+            this.guna2HtmlLabel1.Text = "pumpeks";
+            // 
+            // button2
+            // 
+            this.button2.Animated = true;
+            this.button2.BorderRadius = 15;
+            this.button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(199, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 36);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Dosya Seç";
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // boyut
+            // 
+            this.boyut.Animated = true;
+            this.boyut.BorderRadius = 15;
+            this.boyut.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.boyut.DefaultText = "";
+            this.boyut.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.boyut.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.boyut.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.boyut.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.boyut.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.boyut.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.boyut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.boyut.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.boyut.Location = new System.Drawing.Point(43, 131);
+            this.boyut.Name = "boyut";
+            this.boyut.PasswordChar = '\0';
+            this.boyut.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.boyut.PlaceholderText = "Boyut";
+            this.boyut.SelectedText = "";
+            this.boyut.Size = new System.Drawing.Size(150, 36);
+            this.boyut.TabIndex = 8;
+            this.boyut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // birim
+            // 
+            this.birim.BackColor = System.Drawing.Color.Transparent;
+            this.birim.BorderRadius = 15;
+            this.birim.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.birim.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.birim.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.birim.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.birim.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.birim.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.birim.ItemHeight = 30;
+            this.birim.Items.AddRange(new object[] {
+            "KB",
+            "MB",
+            "GB"});
+            this.birim.Location = new System.Drawing.Point(375, 131);
+            this.birim.Name = "birim";
+            this.birim.Size = new System.Drawing.Size(140, 36);
+            this.birim.StartIndex = 0;
+            this.birim.TabIndex = 9;
+            this.birim.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pompala
+            // 
+            this.pompala.Animated = true;
+            this.pompala.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pompala.BorderRadius = 10;
+            this.pompala.BorderThickness = 1;
+            this.pompala.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.pompala.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.pompala.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.pompala.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.pompala.FillColor = System.Drawing.Color.Transparent;
+            this.pompala.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.pompala.ForeColor = System.Drawing.Color.White;
+            this.pompala.Location = new System.Drawing.Point(177, 234);
+            this.pompala.Name = "pompala";
+            this.pompala.Size = new System.Drawing.Size(219, 38);
+            this.pompala.TabIndex = 10;
+            this.pompala.Text = "Pompala";
+            this.pompala.Click += new System.EventHandler(this.pompalaStart);
+            // 
+            // guna2CircleButton1
+            // 
+            this.guna2CircleButton1.Animated = true;
+            this.guna2CircleButton1.BorderColor = System.Drawing.Color.Gray;
+            this.guna2CircleButton1.CustomImages.Image = global::pumpeks.Properties.Resources.github_PNG40_1417037603;
+            this.guna2CircleButton1.CustomImages.ImageSize = new System.Drawing.Size(25, 25);
+            this.guna2CircleButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButton1.Location = new System.Drawing.Point(12, 273);
+            this.guna2CircleButton1.Name = "guna2CircleButton1";
+            this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButton1.Size = new System.Drawing.Size(46, 46);
+            this.guna2CircleButton1.TabIndex = 12;
+            this.guna2CircleButton1.Click += new System.EventHandler(this.GithubBtn);
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+            this.guna2ControlBox1.ForeColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(532, 12);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 13;
+            // 
+            // ustuneEkle
+            // 
+            this.ustuneEkle.Animated = true;
+            this.ustuneEkle.AutoSize = true;
+            this.ustuneEkle.Checked = true;
+            this.ustuneEkle.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ustuneEkle.CheckedState.BorderRadius = 0;
+            this.ustuneEkle.CheckedState.BorderThickness = 0;
+            this.ustuneEkle.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ustuneEkle.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ustuneEkle.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ustuneEkle.ForeColor = System.Drawing.Color.White;
+            this.ustuneEkle.Location = new System.Drawing.Point(238, 285);
+            this.ustuneEkle.Name = "ustuneEkle";
+            this.ustuneEkle.Size = new System.Drawing.Size(100, 23);
+            this.ustuneEkle.TabIndex = 14;
+            this.ustuneEkle.Text = "Üstüne Ekle";
+            this.ustuneEkle.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.ustuneEkle.UncheckedState.BorderRadius = 0;
+            this.ustuneEkle.UncheckedState.BorderThickness = 0;
+            this.ustuneEkle.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.ustuneEkle.CheckedChanged += new System.EventHandler(this.ustuneEkle_CheckedChanged);
+            // 
+            // menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(589, 331);
+            this.Controls.Add(this.ustuneEkle);
+            this.Controls.Add(this.birim);
+            this.Controls.Add(this.boyut);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.guna2CircleButton1);
+            this.Controls.Add(this.pompala);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2Button button2;
+        private Guna.UI2.WinForms.Guna2TextBox boyut;
+        private Guna.UI2.WinForms.Guna2ComboBox birim;
+        private Guna.UI2.WinForms.Guna2Button pompala;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2CheckBox ustuneEkle;
+    }
+}
+
---
+++ pumpeks/menu.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pumpeks
+{
+    public partial class menu : Form
+    {
+        public menu()
+        {
+            InitializeComponent();
+        }
+
+        // Make window draggable
+        protected override void WndProc(ref Message m)
+        {
+
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+
+        }
+
+        private bool _fileselected = false;
+
+        private void GithubBtn(object sender, EventArgs e)
+        {
+            Process.Start("explorer", "https://github.com/wessly0");
+        }
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+
+                string fileName = openFileDialog1.SafeFileName;
+                button2.Text = fileName;
+                _fileselected = true;
+            }
+
+        }
+
+        private void pompalaStart(object sender, EventArgs e)
+        {
+            if (!_fileselected)
+            {
+                MessageBox.Show("Dosya seçmedin!");
+                return;
+            }
+
+            int Boyut;
+            string Birim;
+
+            if (!int.TryParse(boyut.Text, out Boyut))
+            {
+                MessageBox.Show("boyut alanına sayı girmen gerekiyor.", "?", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            // Hesapla
+            Birim = birim.Text;
+            switch (Birim)
+            {
+                case "KB":
+                    Boyut = Boyut * 1024;
+                    break;
+                case "MB":
+                    Boyut = Boyut * 1024 * 1024;
+                    break;
+                case "GB":
+                    Boyut = Boyut * 1024 * 1024 * 1024;
+                    break;
+                default:
+                    MessageBox.Show("birim seçmemişsin ki.", "?", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+            }
+
+            DialogResult onayla = MessageBox.Show($"Dosyayı {boyut.Text} {birim.Text} {(ustuneEkle.Checked ? "kadar uzatmak" : "olarak ayarlamak")} ister misin?", "Onay", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (onayla == DialogResult.No)
+            {
+                return;
+            }
+
+
+            #region Dosyaya yaz
+
+            var file = File.OpenWrite(openFileDialog1.FileName);
+            var siz = file.Seek(0, SeekOrigin.End);
+
+            // Üstüne ekle açıksa
+            if (ustuneEkle.Checked)
+            {
+                byte[] bytes = new byte[Boyut];
+                file.Write(bytes, 0, bytes.Length);
+            }
+            // Belirtilen boyut kadar olacak
+            else
+            {
+               file.SetLength(Boyut);
+            }
+
+
+            file.Close();
+
+            #endregion
+
+            MessageBox.Show("İşlem tamamlandı. Githubuma göz atmayı unutma!", "Yazıldı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ustuneEkle_CheckedChanged(object sender, EventArgs e)
+        {
+            bool isc = ustuneEkle.Checked;
+            if (isc)
+            {
+                MessageBox.Show("Üstüne ekle açıldığında, belirtilen boyut dosyanın üzerine eklenir.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Üstüne ekle kapalı, dosyanın boyutu belirtilen boyut kadar olacak. Yani üstüne eklenmeyecek, örnek 25MB yazdıysanız dosya tam olarak 25MB olucaktır.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
